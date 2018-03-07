c66cbe681b38c6d5914df6a4fbad6761033d0048
linwei <1194687762@qq.com>
2018-03-07 22:03:41 +0800

    update code

--- music/public/javascripts/index.js
+++ music/public/javascripts/index.js
@@ -19,9 +19,54 @@ var ac = new (window.AudioContext||window.webkitAudioContext)();
 var gainNode = ac.createGain();
 gainNode.connect(ac.destination);
 
+var analyser = ac.createAnalyser();
+var size = 128;
+analyser.fftSize = size * 2;
+analyser.connect(gainNode);
+
 var source = null;
 
 var count = 0;
+
+var box = $("#box")[0];
+var height, width;
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+box.appendChild(canvas);
+
+function resize(){
+    height = box.clientHeight;
+    width = box.clientWidth;
+    canvas.height = height;
+    canvas.width = width;
+    var line = ctx.createLinearGradient(0,0,0,height);
+    line.addColorStop("0","red");
+    line.addColorStop("0.5","yellow");
+    line.addColorStop("1","green");
+    ctx.fillStyle = line;
+}
+resize();
+
+window.onresize = resize;
+
+function draw(arr){
+    ctx.clearRect(0, 0, width, height);
+    var w = width / size;
+    for(var i = 0; i < size; i++){
+        var h = arr[i] / 256 * height;
+        ctx.fillRect(w * i, height - h, w * 0.6, h);
+    }
+}
+
+var types = $("#type li");
+for(var i = 0; i < types.length; i++){
+    types[i].onclick = function(){
+        for(var j = 0; j < types.length; j++){
+            types[j].className = "";
+        }
+        this.className = "selected";
+    }
+}
 
 function load(url){
     var n = ++count;
@@ -36,15 +81,34 @@ function load(url){
             if(n != count) return;
             var bufferSource = ac.createBufferSource();
             bufferSource.buffer = buffer;
-            bufferSource.connect(gainNode);
+            bufferSource.connect(analyser);
             bufferSource[bufferSource.start?"start":"noteOn"](0);
             source = bufferSource;
+            
         },function(err){
             console.log(err);
         });
     }
     xhr.send();
 }
+
+function visualizer(){
+    var arr = new Uint8Array(analyser.frequencyBinCount);
+    
+    requestAnimationFrame = window.requestAnimationFrame ||
+                            window.webkitRequestAnimationFrame ||
+                            window.mozRequestAnimationFrame;
+
+    function v(){
+        analyser.getByteFrequencyData(arr);
+        draw(arr);
+        requestAnimationFrame(v);
+    }
+
+    requestAnimationFrame(v);
+}
+
+visualizer();
 
 function changeVolume(percent){
     gainNode.gain.value = percent * percent;
