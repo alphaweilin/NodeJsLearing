3b836bb0b7772687fa895943045fe0fc887c1bf0
linwei <1194687762@qq.com>
2018-03-08 22:48:22 +0800

    refactor code

--- music/public/javascripts/index.js
+++ music/public/javascripts/index.js
@@ -3,30 +3,7 @@ function $(s){
 }
 
 var array = $("#list li");
-
-for(var i = 0; i < array.length; i++){
-    array[i].onclick = function(){
-        for(var j = 0; j < array.length; j++){
-            array[j].className = "";
-        }
-        this.className = "selected";
-        load("/media/"+this.title);
-    }
-}
-
-var xhr = new XMLHttpRequest();
-var ac = new (window.AudioContext||window.webkitAudioContext)();
-var gainNode = ac.createGain();
-gainNode.connect(ac.destination);
-
-var analyser = ac.createAnalyser();
 var size = 128;
-analyser.fftSize = size * 2;
-analyser.connect(gainNode);
-
-var source = null;
-
-var count = 0;
 
 var box = $("#box")[0];
 var height, width;
@@ -34,6 +11,22 @@ var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 box.appendChild(canvas);
 var dots = [];
+var line;
+
+var mv = new MusicVisualizer({
+    size: size,
+    visualizer: draw
+});
+for(var i = 0; i < array.length; i++){
+    array[i].onclick = function(){
+        for(var j = 0; j < array.length; j++){
+            array[j].className = "";
+        }
+        this.className = "selected";
+        // load("/media/"+this.title);
+        mv.play("/media/"+this.title);
+    }
+}
 
 function random(m,n){
     return Math.round(Math.random()*(n - m) + m);
@@ -51,7 +44,7 @@ function getDots(){
         });
     }
 }
-var line;
+
 function resize(){
     height = box.clientHeight;
     width = box.clientWidth;
@@ -103,54 +96,8 @@ for(var i = 0; i < types.length; i++){
     }
 }
 
-function load(url){
-    var n = ++count;
-    source && source.stop();
-    xhr.abort();
-    xhr.open("GET",url);
-    xhr.responseType = "arraybuffer";
-    xhr.onload = function(){
-        //console.log(xhr.response); 
-        if(n != count) return;
-        ac.decodeAudioData(xhr.response, function(buffer){
-            if(n != count) return;
-            var bufferSource = ac.createBufferSource();
-            bufferSource.buffer = buffer;
-            bufferSource.connect(analyser);
-            bufferSource[bufferSource.start?"start":"noteOn"](0);
-            source = bufferSource;
-            
-        },function(err){
-            console.log(err);
-        });
-    }
-    xhr.send();
-}
-
-function visualizer(){
-    var arr = new Uint8Array(analyser.frequencyBinCount);
-    
-    requestAnimationFrame = window.requestAnimationFrame ||
-                            window.webkitRequestAnimationFrame ||
-                            window.mozRequestAnimationFrame;
-
-    function v(){
-        analyser.getByteFrequencyData(arr);
-        draw(arr);
-        requestAnimationFrame(v);
-    }
-
-    requestAnimationFrame(v);
-}
-
-visualizer();
-
-function changeVolume(percent){
-    gainNode.gain.value = percent * percent;
-}
-
 $("#volume")[0].onchange = function(){
-    changeVolume(this.value/this.max);
+    mv.changeVolume(this.value/this.max);
 }
 //call onchange method
 $("#volume")[0].onchange();
